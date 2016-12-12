1996bab6fc3821f5be107f43be30ec0bb7b1dfe8
szmibartolo <szmibartolo@gmail.com>
2016-12-12 17:47:14 +0100

    Dodałem MainActivity.java i następnie dodałem obsługę Buttona loginButton

--- GPSTracker/app/src/main/java/pl/wsb/szmibartolo/bartosz/gpstracker/LoginActivity.java
+++ GPSTracker/app/src/main/java/pl/wsb/szmibartolo/bartosz/gpstracker/LoginActivity.java
@@ -1,13 +1,31 @@
 package pl.wsb.szmibartolo.bartosz.gpstracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginButton = (Button) findViewById(R.id.loginButton);
+
+        View.OnClickListener loginButtonListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            }
+        };
+
+        loginButton.setOnClickListener(loginButtonListener);
     }
+
+
 }
